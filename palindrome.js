f02630d6c7bbe1ec4147afe09abd1eba34bf6346
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-08 13:58:43 -0800

    changes

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,39 @@
-function palindrome (str) {
-  const result = document.getElementById('result')
-  const inputText = document.getElementById('input-text').value
+function palindrome() {
+  const result = document.getElementById('result');
+  const inputText = document.getElementById('input-text').value;
 
-  // convert string to an array
-  const arrayValues = inputText.split('')
+  function isAlphanumeric(char) {
+    return /^[a-zA-Z0-9]$/.test(char);
+  }
 
-  // reverse the array values
-  const reverseArrayValues = arrayValues.reverse('')
+  // Function to remove all non-alphanumeric characters
+  function removeNonAlphanumeric(str) {
+    let newly = '';
+    for (let i = 0; i < str.length; i++) {
+      if (isAlphanumeric(str[i])) {
+        newly += str[i];
+      }
+    }
+    return newly;
+  }
 
-  // convert array to string
-  const reverseString = reverseArrayValues.join('')
+  // Remove non-alphanumeric characters from input text
+  const newlyInputted = removeNonAlphanumeric(inputText);
 
-  if (inputText === reverseString) {
-    result.innerHTML = 'It is a palindrome'
+  // Convert cleaned input string to an array
+  const arrayValues = newlyInputted.split('');
+
+  // Reverse the array values
+  const reverseArrayValues = arrayValues.reverse();
+
+  // Convert array back to string
+  const reverseString = reverseArrayValues.join('');
+
+  if (newlyInputted === reverseString) {
+    result.innerHTML = 'It is a palindrome';
   } else {
-    result.innerHTML = 'It is not a palindrome'
+    result.innerHTML = 'It is not a palindrome';
   }
 }
-palindrome()
+
+palindrome();
