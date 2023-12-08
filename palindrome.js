89cee442ab784c2199fd90c58ba624e6e016ba4e
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-07 17:02:19 -0800

    change some things palindrome function

--- palindrome.js
+++ palindrome.js
@@ -1,19 +1,19 @@
-function isPalindrome (str){
-    // convert string to an array
-    const arrayValues = str.split('')
+function isPalindrome(str) {
+  // convert string to an array
+  const arrayValues = str.split('')
 
-    // reverse the array values
-    const reverseArrayValues = arrayValues.reverse()
+  // reverse the array values
+  const reverseArrayValues = arrayValues.reverse()
 
-    // convert array to string
-    const reverseString = reverseArrayValues.join('')
+  // convert array to string
+  const reverseString = reverseArrayValues.join('')
 
-    if(str == reverseString) {
-        console.log('It is a palindrome')
-    }
-    else {
-        console.log('It is not a palindrome')
-    }
+  if (str == reverseString) {
+    console.log('It is a palindrome')
+  }
+  else {
+    console.log('It is not a palindrome')
+  }
 }
 //take input
-console.log(isPalindrome('hahahahahhahah'))
+isPalindrome('hah')
