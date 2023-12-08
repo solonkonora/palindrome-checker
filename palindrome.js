be2810db62a2e9c31bcc712fc6bc201f1e10fe91
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-08 10:32:17 -0800

    solving linter errors

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,9 @@
 function palindrome (str) {
   const result = document.getElementById('result')
-  const input_text = document.getElementById('input_text').value
+  const input_text = document.getElementById('input-text').value
 
   // convert string to an array
-  const arrayValues = input_text.split('')
+  const arrayValues = input-text.split('')
 
   // reverse the array values
   const reverseArrayValues = arrayValues.reverse('')
@@ -11,7 +11,7 @@ function palindrome (str) {
   // convert array to string
   const reverseString = reverseArrayValues.join('')
 
-  if (input_text === reverseString) {
+  if (input-text === reverseString) {
     result.innerHTML = 'It is a palindrome'
   } else {
     result.innerHTML = 'It is not a palindrome'
